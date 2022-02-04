b9be7e3b4bff76a0c98ce3d8c2f1b0055647252f
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2022-02-04 11:13:30 +0100

    #17: Template for future implementation of JDBC WebController.

---
+++ src/main/java/kth/iv1201/recruitment/service/AuthenticateService.java
@@ -0,0 +1,16 @@
+package kth.iv1201.recruitment.service;
+
+import kth.iv1201.recruitment.entity.UserForm;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticateService {
+	public int authenticate(UserForm form) {
+		// Person person = personDAO.authenticate(form.getUsername(), form.getPassword());
+		// return person;
+		// TODO send information to backend service.
+		// TODO 'TRUE' return authenticated user.
+		// TODO 'FALSE' return failed authentication.
+		return 0;
+	}
+}
