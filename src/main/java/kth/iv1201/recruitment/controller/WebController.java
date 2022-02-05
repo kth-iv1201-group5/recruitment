d0a075c88e90550d2c952334507a28cb1a92be6d
mrporsev <54756311+mrporsev@users.noreply.github.com>
2022-02-05 20:36:15 +0100

    POST method is handled by the controller. Will insert comments on template of handling requests in controllers.

--- src/main/java/kth/iv1201/recruitment/controller/WebController.java
+++ src/main/java/kth/iv1201/recruitment/controller/WebController.java
@@ -2,6 +2,12 @@ package kth.iv1201.recruitment.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class WebController {
@@ -12,5 +18,10 @@ public class WebController {
     @GetMapping(DEFAULT_PAGE_URL)
     public String showDefaultView() {
         return LOGIN_PAGE_URL;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam("username") final String username, @RequestParam("password") final String password) throws Exception {
+        return "home";
     }
 }
