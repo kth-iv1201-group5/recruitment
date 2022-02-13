d377d3b41ecafcd5efe8a20d3b062fd32c5bd1b3
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-13 15:47:34 +0100

    #32: Constructor injection instead of Field injection. Security +1.

--- src/main/java/kth/iv1201/recruitment/service/PersonService.java
+++ src/main/java/kth/iv1201/recruitment/service/PersonService.java
@@ -2,14 +2,16 @@ package kth.iv1201.recruitment.service;
 
 import kth.iv1201.recruitment.entity.Person;
 import kth.iv1201.recruitment.repository.PersonRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
-	@Autowired
-	private PersonRepository personRepository;
+	private final PersonRepository personRepository;
+
+	public PersonService(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
 	public Person authenticate(String username, String password) {
 		return personRepository.findByUsernameAndPassword(username, password);
