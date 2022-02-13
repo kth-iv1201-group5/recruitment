ca3d9b8937bd78cd35baabfda0e1ab866530ee6f
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-13 15:48:45 +0100

    #32: Constructor injection instead of Field injection. Security +1. Redirect prefix url. Home url.

--- src/main/java/kth/iv1201/recruitment/controller/WebController.java
+++ src/main/java/kth/iv1201/recruitment/controller/WebController.java
@@ -2,7 +2,6 @@ package kth.iv1201.recruitment.controller;
 
 import kth.iv1201.recruitment.entity.Person;
 import kth.iv1201.recruitment.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,22 +11,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
-	static final String DEFAULT_PAGE_URL = "/";
-	static final String LOGIN_PAGE_URL = "/login";
-	@Autowired
-	private PersonService personService;
+	private static final String REDIRECT_PREFIX_URL = "redirect:";
+	private static final String DEFAULT_PAGE_URL = "/";
+	private static final String LOGIN_PAGE_URL = "/login";
+	private static final String HOME_PAGE_URL = "/home";
+
+	private final PersonService personService;
+
+	public WebController(PersonService personService) {
+		this.personService = personService;
+	}
 
 	@GetMapping(DEFAULT_PAGE_URL)
 	public String showDefaultView() {
 		return LOGIN_PAGE_URL;
 	}
 
+	@GetMapping(path = HOME_PAGE_URL)
+	public String home() {
+		return HOME_PAGE_URL;
+	}
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(@RequestParam("username") final String username, @RequestParam("password") final String password) {
+	public String login(@RequestParam(value = "username") final String username, @RequestParam(value = "password") final String password) {
 		Person person = personService.authenticate(username, password);
 		if (person.getUsername() == null && person.getPassword() == null) {
 			return LOGIN_PAGE_URL;
 		}
-		return "home";
+		return REDIRECT_PREFIX_URL + HOME_PAGE_URL;
 	}
 }
