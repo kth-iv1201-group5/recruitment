bc3ac481fd29c63f6cd04449c383c26733b510ce
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-13 11:36:38 +0100

    #30: Remove all setters and only leaving the necessary methods behind. + Column name

--- src/main/java/kth/iv1201/recruitment/entity/Person.java
+++ src/main/java/kth/iv1201/recruitment/entity/Person.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -16,78 +17,23 @@ import javax.persistence.Table;
 @Builder
 @Table(name = "person")
 public class Person {
+	@Id
+	@Column(name = "person_id")
 	private Integer id;
 	private String name;
 	private String surname;
 	private String pnr;
 	private String email;
 	private String password;
-	private Integer roleId;
+	@Column(name = "role_id")
+	private Integer role;
 	private String username;
-
-	@Id
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	public String getPnr() {
-		return pnr;
-	}
-
-	public void setPnr(String pnr) {
-		this.pnr = pnr;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 	public String getPassword() {
 		return password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Integer getRoleId() {
-		return roleId;
-	}
-
-	public void setRoleId(Integer roleId) {
-		this.roleId = roleId;
-	}
-
 	public String getUsername() {
 		return username;
 	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
 }
