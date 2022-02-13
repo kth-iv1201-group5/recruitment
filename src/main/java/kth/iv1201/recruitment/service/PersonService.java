40d59d5502d6a9402d7c85fe3888506fe29e9f73
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-13 16:01:47 +0100

    #32: Javadocs. PersonService.java.

--- src/main/java/kth/iv1201/recruitment/service/PersonService.java
+++ src/main/java/kth/iv1201/recruitment/service/PersonService.java
@@ -4,15 +4,33 @@ import kth.iv1201.recruitment.entity.Person;
 import kth.iv1201.recruitment.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 
+/**
+ * This is our service layer for fetching information from <code>Person</code> database layer.
+ */
 @Service
 public class PersonService {
 
 	private final PersonRepository personRepository;
 
+	/**
+	 * Construction injection.
+	 * <p>This is our way to inject dependencies to the object. This form of injection is called constructor injection.
+	 * Instead of using <code>@Autowired</code>, field inject, which is a security cons. We are relying on construction inject for better testability and immutability.</p>
+	 *
+	 * @param personRepository Construction injecton from <code>PersonRepository</code>
+	 */
 	public PersonService(PersonRepository personRepository) {
 		this.personRepository = personRepository;
 	}
 
+	/**
+	 * Authenticate the user with parameter of username and password coming from the forms.
+	 * <p>This method makes use of <code>PersonRepository</code> to fetch information from the database.</p>
+	 *
+	 * @param username Username of user.
+	 * @param password Password of user
+	 * @return Upon successful it will return a object with filled fields and if not successful it will return empty object.
+	 */
 	public Person authenticate(String username, String password) {
 		return personRepository.findByUsernameAndPassword(username, password);
 	}
