a2bbed7a78594d6cc4a91f25d69eaef70a9f61dc
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-18 00:13:23 +0100

    #38: Repository update and refactoring.

--- src/main/java/kth/iv1201/recruitment/repository/AvailabilityRepository.java
+++ src/main/java/kth/iv1201/recruitment/repository/AvailabilityRepository.java
@@ -7,8 +7,19 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * This object is used for communicating with the database.
+ * It uses the JPARepository as extension as it provides a better ORM to the developer for fetching code.
+ */
 @Repository
 public interface AvailabilityRepository extends JpaRepository<Availability, Integer> {
-	@Query(value = "SELECT OBJECT(a) FROM Availability a WHERE a.personId = ?1")
-	List<Availability> findAllByPersonId(Integer person_id);
+	/**
+	 * This method is for fetching availabilities by their person id.
+	 * This will list all possible availabilities in from the database
+	 *
+	 * @param id Fetching information with id
+	 * @return List of availabilities.
+	 */
+	@Query(value = "SELECT OBJECT(a) FROM Availability a WHERE a.personId = ?1 ORDER BY a.toDate DESC")
+	List<Availability> findAllByPersonId(Integer id);
 }
