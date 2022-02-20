9cb0c9f0855f7194e83d11de2a42f4f90d582251
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-20 23:21:16 +0100

    #29: Add comments on PersonRepository.

--- src/main/java/kth/iv1201/recruitment/repository/PersonRepository.java
+++ src/main/java/kth/iv1201/recruitment/repository/PersonRepository.java
@@ -10,27 +10,42 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 /**
- * This object is used for communicating with the database.
- * It uses the JPARepository as extension as it provides a better ORM to the developer for fetching code.
+ * This object is used for communicating with the database. It uses the JPARepository as extension as it provides a
+ * better ORM to the developer for fetching code.
  */
 @Repository
 @Transactional
 public interface PersonRepository extends JpaRepository<Person, Integer> {
 
 	/**
-	 * This method is for fetching user by their input of username and password.
-	 * This is used for authenticate the user, when user is entering the username and password.
+	 * This method is for fetching user by their input of username and password. This is used for authenticate the
+	 * user,
+	 * when user is entering the username and password.
 	 *
 	 * @param username User entered username.
 	 * @param password User entered password.
+	 *
 	 * @return Person object which may contain information if found or return empty if the query was not successfully.
 	 */
 	@Query(value = "SELECT OBJECT(p) FROM Person p WHERE p.username = :username AND p.password = :password")
 	Person findByUsernameAndPassword(String username, String password);
 
+	/**
+	 * Fetch instance of stored object by their username.
+	 *
+	 * @param username User entered username.
+	 *
+	 * @return Person object which may contain information if found.
+	 */
 	@Query(value = "SELECT p FROM Person p WHERE p.username = :username")
-	Person getUserByUsername(@Param("username") String username); // TODO change 'getUserByUsername' to 'findUserByUsername'
+	Person findByUsername(@Param("username") String username);
 
+	/**
+	 * Fetch all applicants.
+	 * TODO Add 'year' as a parameter
+	 *
+	 * @return All found applicants.
+	 */
 	@Query(value = "SELECT OBJECT(p) FROM Person p WHERE p.role.id = 2")
 	List<Person> findAllApplicants();
 }
