5fa4a9e04d6cb798eceeab433a8f2267779db588
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-20 23:21:28 +0100

    #29: Add comments on PersonService.

--- src/main/java/kth/iv1201/recruitment/service/PersonService.java
+++ src/main/java/kth/iv1201/recruitment/service/PersonService.java
@@ -17,7 +17,8 @@ public class PersonService {
 	/**
 	 * Construction injection.
 	 * <p>This is our way to inject dependencies to the object. This form of injection is called constructor injection.
-	 * Instead of using <code>@Autowired</code>, field inject, which is a security cons. We are relying on construction inject for better testability and immutability.</p>
+	 * Instead of using <code>@Autowired</code>, field inject, which is a security cons. We are relying on construction
+	 * inject for better testability and immutability.</p>
 	 *
 	 * @param personRepository Construction injecton from <code>PersonRepository</code>
 	 */
@@ -31,7 +32,9 @@ public class PersonService {
 	 *
 	 * @param username Username of user.
 	 * @param password Password of user
-	 * @return Upon successful it will return a object with filled fields and if not successful it will return empty object.
+	 *
+	 * @return Upon successful it will return a object with filled fields and if not successful it will return empty
+	 * object.
 	 */
 	public Person authenticate(String username, String password) {
 		return personRepository.findByUsernameAndPassword(username, password);
@@ -41,13 +44,30 @@ public class PersonService {
 	 * Return Person information if found or return empty object.
 	 *
 	 * @param id Person Id.
+	 *
 	 * @return Information of person.
 	 */
 	public Person findById(Integer id) {
 		return personRepository.findById(id).isPresent() ? personRepository.getById(id) : new Person();
 	}
 
+	/**
+	 * Find all possible applicants in database.
+	 *
+	 * @return List of applicants.
+	 */
 	public List<Person> findAllApplicants() {
 		return personRepository.findAllApplicants();
+	}
+
+	/**
+	 * Fetch person information by entered username.
+	 *
+	 * @param username User entered username.
+	 *
+	 * @return Person information.
+	 */
+	public Person findByUsername(String username) {
+		return personRepository.findByUsername(username);
 	}
 }
