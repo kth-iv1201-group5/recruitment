2517fd8992e512ed71a7df2b5078d20facf654b8
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-20 23:22:06 +0100

    #29: Add comments on SecurityUserDetailsService.

--- src/main/java/kth/iv1201/recruitment/service/SecurityUserDetailsService.java
+++ src/main/java/kth/iv1201/recruitment/service/SecurityUserDetailsService.java
@@ -2,7 +2,6 @@ package kth.iv1201.recruitment.service;
 
 import kth.iv1201.recruitment.entity.Person;
 import kth.iv1201.recruitment.entity.SecurityUserDetails;
-import kth.iv1201.recruitment.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,17 +11,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    PersonRepository personRepository;
+	/**
+	 * Autowired service of person service class.
+	 */
+	@Autowired
+	PersonService personService;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Person person = personRepository.getUserByUsername(username);
-
-        if (person == null) {
-            throw new UsernameNotFoundException("Could not find user");
-        }
-
-        return new SecurityUserDetails(person);
-    }
+	/**
+	 * Load User by Username
+	 *
+	 * @param username User entered username.
+	 *
+	 * @return new instance of Security User Details.
+	 *
+	 * @throws UsernameNotFoundException Username could not be found.
+	 */
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Person person = personService.findByUsername(username);
+		if (person == null) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+		return new SecurityUserDetails(person);
+	}
 }
