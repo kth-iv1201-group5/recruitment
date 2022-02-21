8bcb24ab1c78142fe0b3210de504360e37ea32cd
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-02-21 22:14:15 +0100

    #52: Removed '/home' route from authenticated need.

--- src/main/java/kth/iv1201/recruitment/config/SecurityConfig.java
+++ src/main/java/kth/iv1201/recruitment/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/home", "/applicants**").authenticated().and().formLogin().loginPage(
+		http.authorizeRequests().antMatchers("/applicants**").authenticated().and().formLogin().loginPage(
 				"/login").failureUrl("/login-error").permitAll().and().logout().logoutUrl("/logout").clearAuthentication(true).deleteCookies().permitAll();
 	}
 
