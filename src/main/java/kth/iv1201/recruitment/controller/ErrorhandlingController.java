1c839dea0ce64c11a28f696a0ef9cf7e545f8b97
mrporsev <54756311+mrporsev@users.noreply.github.com>
2022-02-24 12:57:43 +0100

    Replaced multiple html-pages with one page that uses Thymeleaf engine and model to become more dynamic

--- src/main/java/kth/iv1201/recruitment/controller/ErrorhandlingController.java
+++ src/main/java/kth/iv1201/recruitment/controller/ErrorhandlingController.java
@@ -3,6 +3,7 @@ package kth.iv1201.recruitment.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -12,23 +13,27 @@ import javax.servlet.http.HttpServletRequest;
 public class ErrorhandlingController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
 
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error-404";
+                model.addAttribute("errorMessage", "The page you've requested does not exist.");
+                model.addAttribute("errorTitle", "Page could not be found");
             }
             else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500";
+                model.addAttribute("errorMessage", "Try to refresh this page or feel free to contact us if the problem persists.");
+                model.addAttribute("errorTitle", "Internal Server Error");
             }
             else if (statusCode == HttpStatus.FORBIDDEN.value()) {
-                return "error-403";
+                model.addAttribute("errorMessage", "You don't have permission to access the requested page.");
+                model.addAttribute("errorTitle", "Forbidden");
             }
             else if(statusCode == HttpStatus.UNAUTHORIZED.value()) {
-                return "error-401";
+                model.addAttribute("errorMessage", "You are not authorized to access the requested page.");
+                model.addAttribute("errorTitle", "Unauthorized");
             }
         }
         return "error";
