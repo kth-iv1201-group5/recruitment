5c2f740056eca68848d16572132d49f78b933209
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-03-07 21:01:22 +0100

    #65: Refactored the email sending.

--- src/main/java/kth/iv1201/recruitment/model/SendMail.java
+++ src/main/java/kth/iv1201/recruitment/model/SendMail.java
@@ -1,60 +1,60 @@
 package kth.iv1201.recruitment.model;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.mail.*;
-import javax.mail.internet.*;
-import java.io.IOException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 import java.util.Properties;
 
-@SpringBootApplication
+@Component
 public class SendMail {
 
-    /**
-     * Consructor for this object
-     */
-    public SendMail(){
+	private final Properties properties;
+	private final String userName;
+	private final String password;
 
-    }
+	/**
+	 * Constructor for this object
+	 */
+	public SendMail(@Value(value = "${smtp.from.user}") String username,
+	                @Value(value = "${smtp.from.password}") String password) {
+		this.userName = username;
+		this.password = password;
+		this.properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+	}
 
-    /**
-     *
-     * @param recipient The reciver of the mail
-     * @param content the Content of the mail
-     * @throws AddressException if address not found
-     * @throws MessagingException
-     * @throws IOException
-     */
-    public void sendmail(String recipient,String content) throws AddressException, MessagingException, IOException {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+	public void sendEmailOfNewPassword(String toEmail, String password) throws Exception {
+		try {
+			Message message = createNewPasswordRequestMessage(toEmail, password);
+			Transport.send(message);
+		} catch (MessagingException e) {
+			throw new Exception("Creating email message was made wrong.");
+		}
+	}
 
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("iv1201.group5@gmail.com", "IV1201++");
-            }
-        });
-        Message msg = new MimeMessage(session);
-        msg.setFrom(new InternetAddress("IV1201.group5@gmail.com", false));
+	private Message createNewPasswordRequestMessage(String toEmail, String password) throws MessagingException {
+		Message message = new MimeMessage(getSession());
+		message.setFrom(new InternetAddress(userName, false));
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+		message.setSubject("[IV1201] Your password have been reset!");
+		message.setContent("<p>You have accessed for a new password.</p>\n\n<p>Your new password is: " + "<b>" + password + "</b>.</p>" + "\n\n<p>Thank you for using our services.</p>", "text/html");
+		message.setSentDate(new Date());
+		return message;
+	}
 
-        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-        msg.setSubject("Password reset");
-        msg.setContent("Your knew password: " + content, "text/html");
-        msg.setSentDate(new Date());
-
-        MimeBodyPart messageBodyPart = new MimeBodyPart();
-        messageBodyPart.setContent("Tutorials point email", "text/html");
-
-
-        Transport.send(msg);
-    }
-
-
-
+	private Session getSession() {
+		return Session.getInstance(this.properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, password);
+			}
+		});
+	}
 }
