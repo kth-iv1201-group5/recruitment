9b1297460c4f0f4ecd34d7ff0a4db298ea830757
Arif Jehda-Oh <3596217-arifjehoh@users.noreply.gitlab.com>
2022-03-12 01:17:50 +0100

    #86: Added method and constructors.

--- src/main/java/kth/iv1201/recruitment/entity/Person.java
+++ src/main/java/kth/iv1201/recruitment/entity/Person.java
@@ -1,9 +1,7 @@
 package kth.iv1201.recruitment.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -14,12 +12,11 @@ import javax.persistence.*;
  */
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 @Table(name = "person")
 public class Person {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "person_id")
 	private Integer id;
 	private String name;
@@ -31,6 +28,53 @@ public class Person {
 	@JoinColumn(name = "role_id")
 	private Role role;
 	private String username;
+
+	public Person() {
+	}
+
+	public Person(String name, String surname, String pnr, String email, String password, String username) {
+		this.name = name;
+		this.surname = surname;
+		this.pnr = pnr;
+		this.email = email;
+		this.password = password;
+		this.username = username;
+	}
+
+	public Person(Integer id, String name, String surname, String pnr, String email, String password, Role role,
+	              String username) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.pnr = pnr;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.username = username;
+	}
+
+	public Person(Person person) {
+		this.id = person.getId();
+		this.name = person.getName();
+		this.surname = person.getSurname();
+		this.pnr = person.getPnr();
+		this.email = person.getEmail();
+		this.password = person.getPassword();
+		this.role = person.getRole();
+		this.username = person.getUsername();
+	}
+
+	public String getPnr() {
+		return pnr;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 	/**
 	 * Returns the password of current object.
@@ -86,6 +130,10 @@ public class Person {
 		return id;
 	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	/**
 	 * Returns the role of user.
 	 *
@@ -93,5 +141,9 @@ public class Person {
 	 */
 	public Role getRole() {
 		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
 	}
 }
